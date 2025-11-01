9e657edfb41c0f41055eaba1608bc78038811d3d
Edwin Castro <edwin.castro@agenciamatch.cl>
2025-10-31 22:40:30 -0500

    feat: add MUI to project :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Button from "@mui/material/Button";
+
+const theme = createTheme({
+  palette: {
+    mode: "light", // Cambia a 'dark' si quieres tema oscuro
+    primary: {
+      main: "#1976d2",
+    },
+  },
+});
+
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">
-        🚀 Tailwind + Tauri + React funcionando
-      </h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "2rem",
+        }}>
+        <Button variant="contained" color="primary">
+          Hola desde MUI 🚀
+        </Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
